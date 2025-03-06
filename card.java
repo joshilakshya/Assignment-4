76446c45ffd0299d8c97bd3b88e24a521f5b832a
Lakshya <89395485+joshilakshya@users.noreply.github.com>
2025-03-06 16:43:19 +0530

    Create card.java

---
+++ card.java
@@ -0,0 +1,24 @@
+class Card {
+    private String rank;
+    private String suit;
+
+    // Constructor
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    
+    // Getters
+    public String getRank() {
+        return rank;
+    }
+    public String getSuit() {
+        return suit;
+    }
+
+    // String representation of a Card
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
